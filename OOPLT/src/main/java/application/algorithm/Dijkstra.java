c2dd3924c6c1e0f1bace2dd7439142b0645b0c46
Nguyễn Đình Thành <163665123+dinhhthanhh@users.noreply.github.com>
2024-12-23 23:52:40 +0700

    v2 update Dijkstra

--- OOPLT/src/main/java/application/algorithm/Dijkstra.java
+++ OOPLT/src/main/java/application/algorithm/Dijkstra.java
@@ -1,98 +1,20 @@
 package application.algorithm;
 
+
+
 import application.model.Edge;
 import application.model.Node;
 
 import java.util.*;
 
-public class Dijkstra {
-    private final Set<Node> nodes;
-    private final boolean directed;
+public class Dijkstra extends GraphAlgorithm {
+    public Dijkstra() {
+        super();  // Gọi constructor của lớp cha
+    }
 
     public Dijkstra(boolean directed) {
         this.directed = directed;
         nodes = new HashSet<>();
-    }
-
-    public void addEdge(Node source, Node destination, double weight) {
-
-        nodes.add(source);
-        nodes.add(destination);
-
-        // We're using addEdgeHelper to make sure we don't have duplicate edges
-        addEdgeHelper(source, destination, weight);
-
-        if (!directed && source != destination) {
-            addEdgeHelper(destination, source, weight);
-        }
-    }
-
-    public void ModifyEdgeWeight(Node a, Node b, double weight) {
-
-        for (Edge edge : a.edges) {
-            if (edge.source == a && edge.destination == b) {
-                // Update the value
-                edge.weight = weight;
-                return;
-            }
-        }
-    }
-
-    public boolean DeleteEd(Node a, Node b) {
-        for (Edge edge : a.edges) {
-            if (edge.source == a && edge.destination == b) {
-                // Update the value in case it's a different one now
-                a.edges.remove(edge);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void DeleteNo(Node from) {
-        for (Node node : nodes) {
-            node.edges.removeIf(edge -> edge.source == from || edge.destination == from);
-        }
-        nodes.remove(from);
-    }
-
-    public void copyEdge(ArrayList<Edge> edges) {
-        for (Node node : nodes) {
-            edges.addAll(node.edges);
-        }
-    }
-
-    private void addEdgeHelper(Node a, Node b, double weight) {
-
-        for (Edge edge : a.edges) {
-            if (edge.source == a && edge.destination == b) {
-                // Update the value in case it's a different one now
-                edge.weight = weight;
-                return;
-            }
-        }
-        // If it hasn't been added already
-        a.edges.add(new Edge(a, b, weight));
-    }
-
-    public boolean hasEdge(Node source, Node destination) {
-        LinkedList<Edge> edges = source.edges;
-        for (Edge edge : edges) {
-            if (edge.destination == destination) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public double Weight(Node source, Node destination) {
-        LinkedList<Edge> edges = source.edges;
-        for (Edge edge : edges) {
-            if (edge.destination == destination) {
-                return edge.weight;
-            }
-        }
-        return 0d;
     }
 
     public void resetNodesVisited() {
@@ -100,10 +22,9 @@ public class Dijkstra {
             node.unvisited();
         }
     }
-
     public String DijkstraShortestPath(Node start, Node end) {
 
-        String output = "";
+        String output ="";
         HashMap<Node, Node> changedAt = new HashMap<>();
         changedAt.put(start, null);
 
@@ -144,6 +65,7 @@ public class Dijkstra {
                     child = parent;
                 }
                 output += path;
+
                 return output;
             }
             currentNode.visit();
@@ -162,7 +84,6 @@ public class Dijkstra {
             }
         }
     }
-
     public Stack<Node> animatePath(Node start, Node end) {
 
         Stack<Node> path = new Stack<>();
@@ -222,7 +143,6 @@ public class Dijkstra {
             }
         }
     }
-
     private Node closestReachableUnvisited(HashMap<Node, Double> shortestPathMap) {
 
         double shortestDistance = Double.POSITIVE_INFINITY;
@@ -242,4 +162,5 @@ public class Dijkstra {
         }
         return closestReachableNode;
     }
+
 }
